50a53a05f95d60d49d1e4edc280f9c43958e11fa
Stefanos7777 <121635517+Stefanos7777@users.noreply.github.com>
2025-05-05 16:16:25 +0300

    Add files via upload

---
+++ project/Main.java
@@ -0,0 +1,119 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        if(args.length < 1) { //if no definition is given it displays a message and terminates
+            System.out.println("No definitions were given.To run the program you have to put the size of the list and next to it the times.The execution is terminated.");
+            return;
+        }
+
+        int size; //the first definition is the size of the list
+        try {
+            size = Integer.parseInt(args[0]);
+        } catch(NumberFormatException e) {
+            System.out.println("The first definition: " + args[0] + " must be a number indicating the size of the list");
+            return;
+        }
+
+        if(args.length - 1 != size) { //match times and sizes
+            System.out.println("The stated size of the list does not match the times given. Execution terminated.");
+            return;
+        }
+
+        List<Integer> initialTimes = new ArrayList<>(); //create the list and add the times from args[]
+        for(int i = 1; i < args.length; i++) {
+            try {
+                initialTimes.add(Integer.parseInt(args[i]));
+            } catch(NumberFormatException e) {
+                System.out.println("The definition: " + args[i] + " is not a valid number.");
+                return;
+            }
+        }
+
+        
+        //we create the first state
+        State FirstState=new State(initialTimes);
+        
+        //definition of the end state
+        State EndState=null;
+
+        List<State> OpenList = new ArrayList<>();
+        Set<State> ClosedList = new HashSet<>();
+
+        //add the first state in open list
+        OpenList.add(FirstState);
+        int sum = initialTimes.stream().mapToInt(Integer::intValue).sum();
+
+
+        while(!OpenList.isEmpty()){ //while the open list isn t empty continue(still have a state to explore)
+
+            State Curr=null;
+
+            
+            for (State state : OpenList) {                         //returns a negative integer, zero, or a positive integer 
+                                                                   //as this object is less than, equal to, or greater than the specified object
+                if (Curr == null || state.compareTo(Curr) < 0) {
+                    Curr = state; //find the state with the lower cost(f)
+                }
+            }
+
+            
+            
+            OpenList.remove(Curr); //remove it
+            
+
+            if(Curr.getG() > sum) { 
+                System.out.println("Total time exceeded "+ sum +" The execution is terminated.");
+                return;
+            }
+
+            if(Curr.isFinal()) { //If its the final state break 
+                EndState = Curr;
+                break;
+            }
+
+            
+            for(State child : Curr.getChildren()) { //find all the childrens
+                
+                //we have to check if we have already explore this state 
+                if(!ClosedList.contains(child) && !OpenList.contains(child)) {
+                    
+                    child.setFather(Curr);
+                    child.evaluate();
+                    
+                    OpenList.add(child);
+                }
+            }
+            ClosedList.add(Curr);
+
+        }
+        if (EndState != null) {
+            
+            LinkedList<State> path = new LinkedList<>();
+            State current = EndState;
+        
+            // construct the path from end to start 
+            while (current != null) {
+                path.addFirst(current);
+                current = current.getFather();
+            }
+        
+            // iterate through the LinkedList and print each state
+            path.forEach(State::print);
+        }else{
+            System.out.println("Didn t find a solution");
+        }
+        
+
+        }
+        
+    }
+
+            
+
+
+
+    
+    
+    
+
---
+++ project/State.java
@@ -0,0 +1,258 @@
+import java.util.*; 
+
+public class State implements Comparable<State>
+{
+	private int f, h, g;
+	private State father;
+	private int totalTime;
+	private List<Integer> RightList; 
+	private List<Integer> LeftList; 
+    private boolean lampOnRight; 
+	List<Integer> initialTimes;
+	
+	
+    
+	
+	//constructor
+    public State(List<Integer> initialTimes) { //takes the times and puts them in RightList
+        this.RightList = new ArrayList<>(initialTimes);
+        this.LeftList = new ArrayList<>();
+        this.f = 0;
+        this.h = 0;
+        this.g = 0;
+        this.father = null;
+        this.totalTime = 0;
+		this.lampOnRight=true; //true on right and false on left
+		this.initialTimes=initialTimes;
+
+    }
+
+	//copy constructor
+	public State(State s) {
+		this.RightList = new ArrayList<>(s.RightList);
+		this.LeftList = new ArrayList<>(s.LeftList);
+		this.f = s.f;
+		this.h = s.h;
+		this.g = s.g;
+		this.father = s.father; 
+		this.totalTime = s.totalTime;
+		this.lampOnRight = s.lampOnRight;
+		this.initialTimes=s.initialTimes;
+	}
+	
+	public List<Integer> getRightList() {
+        return new ArrayList<>(RightList);
+    }
+
+    public void setRightList(List<Integer> RightList) {
+        this.RightList = new ArrayList<>(RightList);
+    }
+
+    public List<Integer> getLeftList() {
+        return new ArrayList<>(LeftList);
+    }
+
+    public void setLeftList(List<Integer> LeftList) {
+        this.LeftList = new ArrayList<>(LeftList);
+    }
+
+	public int MaxH() { //heuristic finds the max of the right list
+		int maxTime = Integer.MIN_VALUE; //minimum possible value for type int
+		for (int time : RightList) {
+			if (time > maxTime) {
+				maxTime = time;
+			}
+		}
+		return maxTime;
+	}
+	
+	
+	public int getF() 
+	{
+		return this.f;
+	}
+	
+	public int getG() 
+	{
+		return this.g;
+	}
+	
+	public int getH() 
+	{
+		return this.h;
+	}
+	
+	public State getFather()
+	{
+		return this.father;
+	}
+	
+	public void setF(int f)
+	{
+		this.f = f;
+	}
+	
+	public void setG(int g)
+	{
+		this.g = g;
+	}
+	
+	public void setH(int h)
+	{
+		this.h = h;
+	}
+	
+	public void setFather(State f)
+	{
+		this.father = f;
+	}
+	
+	public int getTotalTime() 
+	{
+		return this.totalTime;
+	}
+	
+	public void setTotalTime(int time)
+	{
+		this.totalTime = time;
+	}
+	
+	public void evaluate() //calculate f
+	{
+		this.h=MaxH(); 
+		this.f=this.h+this.g;
+	}
+	
+	public void print() {
+		StringBuilder output = new StringBuilder();
+	
+		//construct the string for the left side if it is not empty
+		if (!LeftList.isEmpty()) {
+			StringBuilder leftStrBuilder = new StringBuilder("Left side: ");
+			for (Integer time : LeftList) {
+				leftStrBuilder.append(time).append(", ");
+			}
+			//remove the last comma and space.
+			leftStrBuilder.delete(leftStrBuilder.length() - 2, leftStrBuilder.length());
+			output.append(leftStrBuilder).append(" ");
+		}
+	
+		//construct the string for the right side if it is not empty
+		if (!RightList.isEmpty()) {
+			StringBuilder rightStrBuilder = new StringBuilder("Right side: ");
+			for (Integer time : RightList) {
+				rightStrBuilder.append(time).append(", ");
+			}
+			//remove the last comma and space
+			rightStrBuilder.delete(rightStrBuilder.length() - 2, rightStrBuilder.length());
+			output.append(rightStrBuilder).append(" ");
+		}
+	
+		//compose the torch position string and append it
+		output.append("Torch position: ").append(lampOnRight ? "Right" : "Left").append(" ");
+	
+		//create the elapsed time string and append it.
+		output.append("Time taken: ").append(this.getG());
+	
+		//output the complete status of the crossing.
+		System.out.println(output);
+	}
+	
+
+	
+	public ArrayList<State> getChildren() {
+    	ArrayList<State> children = new ArrayList<>();
+    
+    	if (lampOnRight) {
+        	for (int i = 0; i < RightList.size(); i++) {
+            	for (int j = i + 1; j < RightList.size(); j++) {
+                	List<Integer> newRightList = new ArrayList<>(RightList);
+                	List<Integer> newLeftList = new ArrayList<>(LeftList);
+					
+
+                	Integer item1 = newRightList.get(i);
+                	Integer item2 = newRightList.get(j);
+
+                	newLeftList.add(item1);
+                	newLeftList.add(item2);
+
+                	if (j > i) { //should remove the bigger number
+                    	newRightList.remove(j);
+                    	newRightList.remove(i);
+             		} else {
+                    	newRightList.remove(i);
+                    	newRightList.remove(j);
+                	}
+                
+                	
+					State child = new State(this);
+					child.setRightList(newRightList); 
+					child.setLeftList(newLeftList);  
+					child.setG(child.getG() + Math.max(item1, item2)); 
+					totalTime+=Math.max(item1, item2);
+					child.lampOnRight = !this.lampOnRight;
+					children.add(child);
+                	
+            	}
+        	}
+    	} else {
+        	for (int i = 0; i < LeftList.size(); i++) {
+         		List<Integer> newRightList = new ArrayList<>(RightList);
+            	List<Integer> newLeftList = new ArrayList<>(LeftList);
+				
+
+            	Integer item = newLeftList.get(i);
+            	newRightList.add(item);
+            	newLeftList.remove(i);
+
+				State child = new State(this);
+				child.setRightList(newRightList);
+				child.setLeftList(newLeftList);   
+				child.setG(child.getG() + item); 
+				totalTime+=item;
+				child.lampOnRight = !this.lampOnRight;
+            	children.add(child);
+        	}
+    	}
+
+    	return children;
+		
+       
+	}
+
+	
+	
+	public boolean isFinal() {
+		return RightList.isEmpty() && lampOnRight==false; //if all the persons and the lamp are on the left side
+	} 
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj){ 
+			return true;
+		} 
+		if (obj == null || getClass() != obj.getClass()) {  
+			return false;
+		}
+		State state = (State) obj; 
+		return lampOnRight == state.lampOnRight && 
+				Objects.equals(LeftList, state.LeftList) &&
+				Objects.equals(RightList, state.RightList);
+			}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(lampOnRight, LeftList, RightList);
+	}
+			
+	
+	@Override
+    public int compareTo(State s)
+    {
+        return Double.compare(this.f, s.getF()); // compare based on the heuristic score.
+    }
+
+
+	
+}
+
